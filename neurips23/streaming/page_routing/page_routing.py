9d952e8dd47afd2e492c558919a6d55419f4cc16
impanyu <impanyu@gmail.com>
2023-08-11 09:14:18 -0500

    test

--- neurips23/streaming/page_routing/page_routing.py
+++ neurips23/streaming/page_routing/page_routing.py
@@ -13,6 +13,7 @@ class PageRouting(BaseStreamANN):
         '''
         raise NotImplementedError
         
+        
     def insert(self, X: np.array, ids: npt.NDArray[np.uint32]) -> None:
         '''
         Implement this for your algorithm
