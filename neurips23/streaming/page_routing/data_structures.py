919ac1a84495ac0ad2d8508732679041a0bc616b
impanyu <impanyu@gmail.com>
2023-08-28 11:12:57 -0500

    add readwritelock

--- neurips23/streaming/page_routing/data_structures.py
+++ neurips23/streaming/page_routing/data_structures.py
@@ -6,6 +6,37 @@ import random
 import os
 import threading
 
+class ReadWriteLock:
+    def __init__(self):
+        self._read_ready = threading.Condition(threading.Lock())
+        self._readers = 0
+
+    def acquire_read(self):
+        """Acquire a read lock. Multiple threads can hold this typeof lock.
+        It is exclusive with write locks."""
+        with self._read_ready:
+            self._readers += 1
+
+    def release_read(self):
+        """Release a read lock."""
+        with self._read_ready:
+            self._readers -= 1
+            if self._readers == 0:
+                self._read_ready.notifyAll()
+
+    def acquire_write(self):
+        """Acquire a write lock. Only one thread can hold this lock,
+        and only when no read locks are also held."""
+        self._read_ready.acquire()
+        while self._readers > 0:
+            self._read_ready.wait()
+
+    def release_write(self):
+        """Release a write lock."""
+        self._read_ready.release()
+
+
+
 
 class Node:
     def __init__(self, vector, node_id,page_index, max_neighbors=50, alpha=1.2):
