e16fbeba53754b514daf9d44554c4d8ebdf2b75a
impanyu <impanyu@gmail.com>
2023-09-25 16:30:15 -0500

    test

--- neurips23/streaming/low_memory/data_structures2.py
+++ neurips23/streaming/low_memory/data_structures2.py
@@ -350,8 +350,8 @@ class low_memory_index:
         #self.index_file_rw_lock.acquire_read()
         with open(self.index_file, 'rb') as f:
             # index_file is a binary file, so we need to seek to the correct position
-            print(node_id *self.node_size*4)
-            f.seek(node_id)
+            print(node_id )
+            f.seek(node_id *self.node_size*4)
             # read the node from the file
             try:
                 node_data = np.fromfile(f, dtype=np.float32, count=int(self.node_size))
