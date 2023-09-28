40b8ee1cee23e68c4badd965facc401d6543e891
impanyu <impanyu@gmail.com>
2023-09-28 12:04:23 -0500

    test

--- neurips23/streaming/low_memory/data_structures2.py
+++ neurips23/streaming/low_memory/data_structures2.py
@@ -72,7 +72,7 @@ class Node:
         
         #print(f"number of clusters: {len(clusters)}")
         #print(clusters)
-        print(self.neighbor_ids)
+        #print(self.neighbor_ids)
         print(self.node_id)
         if len(clusters) != self.max_cluster_number:
             print("cluster number not equal to max cluster number")
