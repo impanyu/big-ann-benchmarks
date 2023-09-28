193ff34b8c7bce5c3505d1cd819f20438c0f8cd9
impanyu <impanyu@gmail.com>
2023-09-28 16:27:06 -0500

    test

--- neurips23/streaming/low_memory/data_structures2.py
+++ neurips23/streaming/low_memory/data_structures2.py
@@ -610,8 +610,9 @@ class low_memory_index:
             
 
             current_node = self.get_node(current_node_id)
-            to_visit_distances[current_node_id] = current_node.get_distance(query_vector)
-            
+            to_visit_distances[current_node_id][0] = current_node.get_distance(query_vector)
+            to_visit_distances[current_node_id][1] = current_node.get_distance(query_vector)
+
             neighbor_ids = current_node.get_neighbor_ids()
 
             for neighbor_id in neighbor_ids:
